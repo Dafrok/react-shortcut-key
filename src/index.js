49277ecbd622a11cae00a88f8a0f32e05e0c2617
Dafrok <o.o@mug.dog>
2016-09-24 22:10:56 +0800

    Use higher-order functions

--- src/index.js
+++ src/index.js
@@ -1,45 +1,48 @@
 import React from 'react'
 
-export default class ShortcutKey
- extends React.Component {
-    constructor(props) {
+export default function (keymap) {
+  return function (Comp) {
+    return class ShortcutKey extends React.Component {
+      constructor (props) {
         super(props)
-    }
-    executeShortcut (e, options = {}) {
+        this.keyHandler = this.keyHandler.bind(this)
+        this.executeShortcut = this.executeShortcut.bind(this)
+      }
+      executeShortcut (e, options = {}) {
         if (e.keyCode === options.keyCode
-            && e.target.tagName === 'BODY'
-            && (typeof options.ctrl !== 'undefined' ? options.ctrl ? e.ctrlKey : !e.ctrlKey : true)
-            && (typeof options.alt !== 'undefined' ? options.alt ? e.altKey : !e.altKey : true)
-            && (typeof options.shift !== 'undefined'? options.shift ? e.shiftKey : !e.shiftKey : true)) {
-            options.prevent && e.preventDefault()
-            options.stop && e.stopPropagation()
-            options.fn()
+          && e.target.tagName === 'BODY'
+          && (typeof options.ctrl !== 'undefined' ? options.ctrl ? e.ctrlKey : !e.ctrlKey : true)
+          && (typeof options.alt !== 'undefined' ? options.alt ? e.altKey : !e.altKey : true)
+          && (typeof options.shift !== 'undefined'? options.shift ? e.shiftKey : !e.shiftKey : true)) {
+          options.prevent && e.preventDefault()
+          options.stop && e.stopPropagation()
+          options.fn()
         }
-    }
-    keyHandler = e => {
+      }
+      keyHandler (e) {
         const _exec = this.executeShortcut.bind(this, e)
         const keymap = this.props.keymap
         for (const name in this.props.keymap) {
-            _exec({
-                keyCode: keymap[name].keyCode,
-                fn: keymap[name].fn,
-                ctrl: keymap[name].ctrl,
-                alt: keymap[name].alt,
-                shift: keymap[name].shift,
-                stop: keymap[name].stop,
-                prevent: keymap[name].prevent
-            })
+          _exec({
+            keyCode: keymap[name].keyCode,
+            fn: keymap[name].fn,
+            ctrl: keymap[name].ctrl,
+            alt: keymap[name].alt,
+            shift: keymap[name].shift,
+            stop: keymap[name].stop,
+            prevent: keymap[name].prevent
+          })
         }
-    }
-    componentDidMount () {
+      }
+      componentDidMount () {
         document.addEventListener('keydown', this.keyHandler)
-    }
-    componentWillUnmount () {
+      }
+      componentWillUnmount () {
         document.removeEventListener('keydown', this.keyHandler)
+      }
+      render () {
+        return <Comp {...this.props}/>
+      }
     }
-    render () {
-        return (<span>
-            {this.props.children}
-        </span>)
-    }
+  }
 }
